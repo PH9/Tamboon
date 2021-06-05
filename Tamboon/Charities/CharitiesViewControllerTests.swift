4667e5caf5c6ceeaf4df427566a29af7a0881ec1
Wasith 'Pi' Theerapattrathamrong <wasith.t@gmail.com>
2021-06-05 22:09:25 +0700

    refactor: since all dependencies should be stubbed
    and sut was use many times. Try to setup it once but every time test
    case running.

--- Tamboon/Charities/CharitiesViewControllerTests.swift
+++ Tamboon/Charities/CharitiesViewControllerTests.swift
@@ -2,19 +2,22 @@
 import XCTest
 
 final class CharitiesViewControllerTests: XCTestCase {
-  func test_charitiesViewController_dataSource_shouldBeCharitiesDataSource() {
-    let sut = CharitiesViewController()
+  let sut = CharitiesViewController()
+  var interactorSpy: CharitiesInteractorSpy!
 
+  override func setUp() {
+    super.setUp()
+    interactorSpy = CharitiesInteractorSpy()
+    sut.interactor = interactorSpy
+  }
+
+  func test_charitiesViewController_dataSource_shouldBeCharitiesDataSource() {
     sut.viewDidLoad()
 
     XCTAssert(sut.tableView.dataSource is CharitiesDataSource)
   }
 
-  func test_viewDidLoad_shouldCallGetCharities() {
-    let sut = CharitiesViewController()
-    let interactorSpy = CharitiesInteractorSpy()
-    sut.interactor = interactorSpy
-
+  func test_viewDidLoad_shouldCallGetCharitiesOnce() {
     _ = sut.view
 
     XCTAssertEqual(interactorSpy.getCharitiesCalledCount, 1)
